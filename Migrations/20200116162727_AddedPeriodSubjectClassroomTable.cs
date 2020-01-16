8e24acf7020714d47783c222f686b867ee0d1c41
Justin Swiger <justin.swiger@me.com>
2020-01-16 11:28:15 -0500

    updated classroom model

---
+++ Migrations/20200116162727_AddedPeriodSubjectClassroomTable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RewardTrackerAPI.Migrations
+{
+    public partial class AddedPeriodSubjectClassroomTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Classrooms");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PeriodNumber",
+                table: "Classrooms",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Classrooms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PeriodNumber",
+                table: "Classrooms");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Classrooms");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Classrooms",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- Models/Classroom.cs
+++ Models/Classroom.cs
@@ -5,6 +5,8 @@ namespace RewardTrackerAPI.Models
   public class Classroom
   {
     public int Id { get; set; }
+    public int PeriodNumber { get; set; }
+    public string Subject { get; set; }
     public int TeacherId { get; set; }
     public Teacher Teacher { get; set; }
     public List<Student> Students { get; set; } = new List<Student>();

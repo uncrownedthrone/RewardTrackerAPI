f3bb2caa2e6522ee0e654295d619302105769dac
Justin Swiger <justin.swiger@me.com>
2020-01-17 11:17:46 -0500

    added reward record to reward table

---
+++ Migrations/20200117161717_UpdatedRewardTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RewardTrackerAPI.Migrations
+{
+    public partial class UpdatedRewardTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RewardRecord",
+                table: "Rewards",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RewardRecord",
+                table: "Rewards");
+        }
+    }
+}
--- Models/Reward.cs
+++ Models/Reward.cs
@@ -3,6 +3,7 @@ namespace RewardTrackerAPI.Models
   public class Reward
   {
     public int Id { get; set; }
+    public int RewardRecord { get; set; }
     public int StudentId { get; set; }
     public Student Student { get; set; }
     public int TeacherId { get; set; }

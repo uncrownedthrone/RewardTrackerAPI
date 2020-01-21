99a051266193323e09a72b3171c6c5e0ff7e219f
Justin Swiger <justin.swiger@me.com>
2020-01-21 11:30:35 -0500

    added studentdetails vm

---
+++ ViewModels/StudentDetails.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace RewardTrackerAPI.ViewModels
+{
+  public class StudentDetails
+  {
+    public string Name { get; set; }
+    public string House { get; set; }
+    public int PeriodId { get; set; }
+
+  }
+}

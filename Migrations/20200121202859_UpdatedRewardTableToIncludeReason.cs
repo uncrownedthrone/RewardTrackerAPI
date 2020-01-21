056ce0308beff2194bbd2e6ef228d79fb7ea5224
Justin Swiger <justin.swiger@me.com>
2020-01-21 15:29:16 -0500

    ran migrations

---
+++ Migrations/20200121202859_UpdatedRewardTableToIncludeReason.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RewardTrackerAPI.Migrations
+{
+    public partial class UpdatedRewardTableToIncludeReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "RewardRecords",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "RewardRecords");
+        }
+    }
+}

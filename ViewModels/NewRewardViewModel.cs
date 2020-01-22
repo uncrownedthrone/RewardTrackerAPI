55b422c09320d92e95ed2c34e4aa6cb036111011
Justin Swiger <justin.swiger@me.com>
2020-01-22 11:43:48 -0500

    added newreward vm

---
+++ ViewModels/NewRewardViewModel.cs
@@ -0,0 +1,9 @@
+namespace RewardTrackerAPI.ViewModels
+{
+  public class RewardDetails
+  {
+    public string Reason { get; set; }
+    public int RewardAmount { get; set; }
+    public int StudentId { get; set; }
+  }
+}

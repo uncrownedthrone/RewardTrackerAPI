f14c6feab4e4feb35ca9c966218d9424f2d07e5a
Justin Swiger <justin.swiger@me.com>
2020-01-22 09:54:34 -0500

    added reward amount to reward table

---
+++ Migrations/20200122145411_AddedRewardAmount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RewardTrackerAPI.Migrations
+{
+    public partial class AddedRewardAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RewardAmount",
+                table: "RewardRecords",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RewardAmount",
+                table: "RewardRecords");
+        }
+    }
+}
--- Models/Reward.cs
+++ Models/Reward.cs
@@ -6,6 +6,7 @@ namespace RewardTrackerAPI.Models
   {
     public int Id { get; set; }
     public string Reason { get; set; }
+    public int RewardAmount { get; set; }
     public int StudentId { get; set; }
     public Student Student { get; set; }
     public int TeacherId { get; set; }
